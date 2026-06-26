421f4012952cf65fde30792cf865e133fcc532ab
khadijaidbihi1-ops <khadijaidbihi1@gmail.com>
2026-06-26 16:31:32 +0000

    creation clinics table

--- database_nhs.sql
+++ database_nhs.sql
@@ -1,52 +1,46 @@
 --   Table NHS Databaase
 CREATE DATABASE NHS_Trust_DB;
-
 USE NHS_Trust_DB;
  
 -- 1. Create Medications Table
 CREATE TABLE Medications (
-
     MedicationID BIGINT NOT NULL AUTO_INCREMENT,       
-
     MedicationName VARCHAR(150) NOT NULL,
-
     Descritption TEXT,
-
     -- Define primary key
-
     PRIMARY KEY (MedicationID)
-
 );
 
 
 -- 2. Create Patients Table
-
 CREATE TABLE Patients (
-
     PatientID BIGINT NOT NULL AUTO_INCREMENT,
-
     PatientName VARCHAR(50) NOT NULL,
-
     PatientSurname VARCHAR(50) NOT NULL,
-
     PhoneNumber BIGINT NOT NULL,
-
     Email VARCHAR(100) NOT NULL,
-
     NHSnumber BIGINT NOT NULL,
-
     Address VARCHAR(255),
-
     DateBirth DATE NOT NULL,
-
     ActiveOrNot BOOLEAN DEFAULT TRUE,
-
-    -- Define primary key and unique constraint
-
+    -- Define primary key
     PRIMARY KEY (PatientID),
-
+    -- Define unique constraint to avoid duplication of patients with the same name, 
+    --surname, and date of birth
     UNIQUE KEY `unique_patient` (`PatientName`,`PatientSurname`,`DateBirth`),
-
+    -- constaint on email format
     CONSTRAINT `chk_patient_email` CHECK ((`Email` like '%@%.%'))
+);
 
+-- 3. Create Clinics Table 
+CREATE TABLE Clinics (
+    ClinicID BIGINT NOT NULL AUTO_INCREMENT,
+    ClinicName VARCHAR(150) NOT NULL,
+    ClinicAddress VARCHAR(255),
+    PhoneNumber BIGINT NOT NULL,
+    Email VARCHAR(100),
+    -- Define primary key
+    PRIMARY KEY (ClinicID),
+    -- constaint on email format
+    CONSTRAINT `chk_clinic_email` CHECK (`Email` like '%@%.%')
 );
